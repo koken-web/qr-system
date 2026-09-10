56e39384dc7a305ac6992583997a57cdbd07aec0
1453manamana5-web <1453manamana5@gmail.com>
2026-09-10 18:48:35 +0900

    Revert duplicate authentication helper

--- src/firebaseAuth.ts
+++ src/firebaseAuth.ts
@@ -1,8 +1,5 @@
 import {
   getAuth,
-  onAuthStateChanged,
-  signInAnonymously,
-  type User,
 } from "firebase/auth";
 
 import {
@@ -11,61 +8,3 @@ import {
 
 export const auth =
   getAuth(firebaseApp);
-
-let authenticationPromise:
-  Promise<User> | null = null;
-
-export function ensureFirebaseAuthentication() {
-  if (auth.currentUser !== null) {
-    return Promise.resolve(auth.currentUser);
-  }
-
-  if (authenticationPromise !== null) {
-    return authenticationPromise;
-  }
-
-  authenticationPromise =
-    new Promise<User>((resolve, reject) => {
-      let unsubscribe: (() => void) | null = null;
-      let settled = false;
-
-      const finish = (
-        callback: () => void
-      ) => {
-        if (settled) {
-          return;
-        }
-
-        settled = true;
-        unsubscribe?.();
-        callback();
-      };
-
-      unsubscribe = onAuthStateChanged(
-        auth,
-        (user) => {
-          if (user !== null) {
-            finish(() => resolve(user));
-          }
-        },
-        (error) => {
-          finish(() => reject(error));
-        }
-      );
-
-      if (auth.currentUser !== null) {
-        finish(() =>
-          resolve(auth.currentUser as User)
-        );
-        return;
-      }
-
-      void signInAnonymously(auth).catch((error) => {
-        finish(() => reject(error));
-      });
-    }).finally(() => {
-      authenticationPromise = null;
-    });
-
-  return authenticationPromise;
-}
